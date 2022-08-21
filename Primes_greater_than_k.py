ba6569a45659aefb61158e57a9e8d347dfe2e60f
Surya Reddi <84438121+20A91A04O3@users.noreply.github.com>
2022-08-21 23:18:55 +0530

    def isprime(a):     if a>1:         for i in range(2,a):             if a%i==0:                 return False         else:             return True     else:         return False n=int(input()) l=list(map(int,input().split())) k=int(input()) c=0 for i in l:     if (isprime(i)==True) and (i>=k):         c+=1 print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,17 @@
+def isprime(a):
+    if a>1:
+        for i in range(2,a):
+            if a%i==0:
+                return False
+        else:
+            return True
+    else:
+        return False
+n=int(input())
+l=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in l:
+    if (isprime(i)==True) and (i>=k):
+        c+=1
+print(c)
