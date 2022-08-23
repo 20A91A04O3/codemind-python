16da7273e7280b7e279a454f6abc3b2b8dd89839
Surya Reddi <84438121+20A91A04O3@users.noreply.github.com>
2022-08-23 11:23:58 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] for i in l:     c=0     while(i>9):         r=i%10         c+=1         i=i//10     l1.append(c) e=l1.count(min(l1)) print(e)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+for i in l:
+    c=0
+    while(i>9):
+        r=i%10
+        c+=1
+        i=i//10
+    l1.append(c)
+e=l1.count(min(l1))
+print(e)
