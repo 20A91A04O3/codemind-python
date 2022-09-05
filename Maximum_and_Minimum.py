1e2a36c183a99ec4df3b0476e970d3a7aa1b8d4c
Surya Reddi <84438121+20A91A04O3@users.noreply.github.com>
2022-09-05 14:44:26 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] s=set(l) for i in s:     if i==l.count(i):         l1.append(i) if len(l1)!=0:     print(min(l1),max(l1)) else:     print(-1)

--- Maximum_and_Minimum.py
+++ Maximum_and_Minimum.py
@@ -1,16 +1,11 @@
 n=int(input())
 l=list(map(int,input().split()))
-dic={}
 l1=[]
-for i in l:
-    if i not in dic:
-        dic[i]=1
-    else:
-        dic[i]+=1
-for k,v in dic.items():
-    if k==v:
-        l1.append(k)
-if len(l1)!=0 :
+s=set(l)
+for i in s:
+    if i==l.count(i):
+        l1.append(i)
+if len(l1)!=0:
     print(min(l1),max(l1))
 else:
     print(-1)
