8c2e75e72fa9a6021a661dafadaa885d0472e077
Surya Reddi <84438121+20A91A04O3@users.noreply.github.com>
2022-09-18 17:45:11 +0530

    s=input() c=0 for i in s:     if (i.isalpha()!=True) and (i.isdigit()!=True) and (i!=" "):         c=c+1 print(c)

--- special_characters_count.py
+++ special_characters_count.py
@@ -1,6 +1,6 @@
 s=input()
 c=0
 for i in s:
-    if (i.isalpha()!=True) and(i.isdigit()!=True) and i!=" ":
-        c+=1
+    if (i.isalpha()!=True) and (i.isdigit()!=True) and (i!=" "):
+        c=c+1
 print(c)
