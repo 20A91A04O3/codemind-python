5fad253ccfea24aefcba6ed9bde6389d8fb1547b
Surya Reddi <84438121+20A91A04O3@users.noreply.github.com>
2022-09-19 19:33:22 +0530

    s1=input() s2=s1.lower() s3=s2.split() s4=''.join(s3) s5=set(s4) l=[] for i in s5:     c=0     for j in s3:         if i in j:             c+=1             if c==len(s3):                 l.append(i) if len(l)!=0:     print(min(l)) else:     print(-1)

---
+++ Common_characters_-_3.py
@@ -0,0 +1,17 @@
+s1=input()
+s2=s1.lower()
+s3=s2.split()
+s4=''.join(s3)
+s5=set(s4)
+l=[]
+for i in s5:
+    c=0
+    for j in s3:
+        if i in j:
+            c+=1
+            if c==len(s3):
+                l.append(i)
+if len(l)!=0:
+    print(min(l))
+else:
+    print(-1)
