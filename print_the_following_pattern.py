3d97ee56436b5cb030de6059c813a1bb7b4cd938
Surya Reddi <84438121+20A91A04O3@users.noreply.github.com>
2022-10-10 08:43:26 +0530

    n=int(input()) s=[] for i in range(1,n+1):     for j in range(n,0,-1):         print(j,end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,9 +1,6 @@
 n=int(input())
-for i in range(n):
-    s=''
-    for j in range(n):
-        if i==j:
-            s+="0"
-        else:
-            s+="x"
-    print(s)
+s=[]
+for i in range(1,n+1):
+    for j in range(n,0,-1):
+        print(j,end=' ')
+    print()
